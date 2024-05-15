c71d1bcf7ed995593d7377c82a1daec7d3c8b7a4
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-05-15 19:05:12 +0200

    Creating interface planets with the structure of a planet

---
+++ src/app/models/planets.model.ts
@@ -0,0 +1,22 @@
+export interface Planets {
+  name:        string;
+  overview:    Geology;
+  structure:   Geology;
+  geology:     Geology;
+  rotation:    string;
+  revolution:  string;
+  radius:      string;
+  temperature: string;
+  images:      Images;
+}
+
+export interface Geology {
+  content: string;
+  source:  string;
+}
+
+export interface Images {
+  planet:   string;
+  internal: string;
+  geology:  string;
+}
