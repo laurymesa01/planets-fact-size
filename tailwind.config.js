8cbf35bd07f799b8b78a20cff8125e507df74662
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-05-15 17:39:49 +0200

    Setting global colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'white': '#FFFFFF',
+      'dark-blue': '#070724',
+      'dark-grey': '#38384F',
+      'light-grey': '#838391',
+      'blue-green': '#419EBB',
+      'yellow-mustard': '#EDA249',
+      'dark-violet': '#6f2ed6',
+      'light-orange': '#D14C32',
+      'red': '#D83A34',
+      'orange': '#CD5120',
+      'green': '#1ec2a4',
+      'blue': '#2d68f0'
+    },
+    fontFamily: {
+      'antonio': 'Antonio',
+      'spartan': 'Spartan'
+    }
   },
   plugins: [],
 }
